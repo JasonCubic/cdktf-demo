348e23c59e252a85b34b71cbdde953f89d47df56
Jason Cubic <jcubic@gmail.com>
2023-01-10 14:55:32 -0800

    minor updates

--- src/constructs/azurerm-oidc-provider/index.ts
+++ src/constructs/azurerm-oidc-provider/index.ts
@@ -1,16 +1,12 @@
-import { AzurermProviderConfig } from '@cdktf/provider-azurerm/lib/provider/index.js';
+import { AzurermProvider, AzurermProviderConfig } from '@cdktf/provider-azurerm/lib/provider/index.js';
 import { Construct } from 'constructs';
 
-import { provider } from '@cdktf/provider-azurerm';
-
-// All of the classes and types are available in @cdktf/provider-azurerm
 // this modifies the AzurermProviderConfig type to require that OIDC be set to true
 interface AzurermOidcProviderConfig extends AzurermProviderConfig { useOidc: true }
 
-class AzurermOidcProvider extends Construct {
+class AzurermOidcProvider extends AzurermProvider {
   constructor(scope: Construct, name: string, options: AzurermOidcProviderConfig) {
-    super(scope, name);
-    new provider.AzurermProvider(this, 'azurerm', options);
+    super(scope, name, (options as AzurermProviderConfig));
   }
 }
 
